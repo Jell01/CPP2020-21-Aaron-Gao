a31b04a7a988a6f3ba36a2fe15a70bb4e03f267f
Jell01 <67522272+Jell01@users.noreply.github.com>
2020-10-20 14:26:29 -0700

    Add files via upload

--- Classes/Media.h
+++ Classes/Media.h
@@ -13,16 +13,6 @@ class Media {
   void setYear(int newYear);
   char* getTitle();
   void setTitle(char* newTitle);
-  virtual char* getPublisher();
-  virtual void setPublisher(char* newPublisher);
-  virtual float getRating();
-  virtual void setRating(int newRating);
-  virtual char* getArtist();
-  virtual void setArtist(char* newArtist);
-  virtual int getDuration();
-  virtual void setDuration(int newDuration);
-  virtual char* getDirector();
-  virtual void setDirector(char* newDirector);
 
  private:
   int year;
